b4f0c13c481024154a809c3eb5b511fe3ece5637
PromotezCitizen <gkstkdgus821@gmail.com>
2025-02-04 00:28:46 +0900

    댓글 view를 컴포넌트에서 부모로 이동

--- src/views/CommentsView.vue
+++ src/views/CommentsView.vue
@@ -1,35 +1,56 @@
 <script setup lang="ts">
-import Comment from '@/components/Comment.vue';
 import router from '@/router';
 import { useAuthStore } from '@/stores/auth';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
+import { tokenAxiosInstance } from '@/utils';
+import type { CommentResource, CommentResponseData } from "@/types/comment-interface";
+import type { InterVideoListNComment } from "@/types/video-interface";
 
-const params = history.state;
-const videoId: string = params.videoId as string;
+const state = history.state as InterVideoListNComment;
 const authStore = useAuthStore();
+
+const items = ref<CommentResource[]>([]);
+const isLast = ref<boolean>(false);
+
+const fetchComments = async (): Promise<CommentResponseData> => {
+    const response = await tokenAxiosInstance.get<CommentResponseData>(`/api/youtube/videos/${state.video.id}`, {
+        withCredentials: true,
+        params: {
+            page: 1,
+            take: 100,
+        }
+    })
+    return response.data;
+}
 
 onMounted(() => {
     // if (!authStore.isLoggedIn) {
     //     alert("로그인을 먼저 해주세요")
     //     router.replace("/");
     // }
+
+    fetchComments()
+        .then((data) => {
+            console.log(data)
+            isLast.value = data.isLast;
+            items.value = [...items.value, ...data.items];
+        })
+        .catch((err) => console.log(err));
 })
 
 </script>
 
 <template>
-    <Comment :videoId="videoId">
-        <template #item="{id, authorNickname, textOriginal}">
-            <div class="item">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="trash-icon">
-                    <path d="M3 6L5 6L21 6L21 8L5 8L5 20C5 20.55 5.45 21 6 21H18C18.55 21 19 20.55 19 20V8L21 8L21 6L3 6Z"/>
-                </svg>
-                <p> comment id: {{ id }}</p>
-                <p> {{ authorNickname }}</p>
-                <p class="meta"> {{ textOriginal }}</p>
-            </div>
-        </template>
-    </Comment>
+    <div>
+        <li v-if="!items.length">
+            Loading..
+        </li>
+        <div v-for="(item, index) in items" :key="index" class="item">
+            <p> comment id: {{ item.id }}</p>
+            <p> {{ item.nickname }}({{ item.nicknamePredict }})</p>
+            <p class="meta"> {{ item.comment }}({{ item.commentPredict }})</p>
+        </div>
+    </div>
 </template>
 
 <style scoped>
