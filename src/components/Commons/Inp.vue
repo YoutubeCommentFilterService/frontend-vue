b1ece7df95913740bbb7d30804f6ffda1b0d729d
stonecat <stonecat52@naver.com>
2025-02-10 00:04:00 +0900

    [ADD] Set Component

---
+++ src/components/Commons/Inp.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  placeholder: String,
+  onchange: Function
+});
+
+const handleChange = (event: Event) => {
+  if (props.onchange) {
+    props.onchange(event);
+  }
+};
+</script>
+
+<template>
+  <input
+    :placeholder="props.placeholder"
+    @change="handleChange"
+  />
+</template>
+
+<style scoped>
+  input {
+    padding: 0.8rem;
+
+    width: 100%;
+    max-width: 33rem;
+
+    font-size: 1rem;
+
+    border: 2px solid #ccc;
+    border-radius: 12px;
+
+    outline: none;
+    transition: border-color 0.3s ease;
+  }
+
+  input:focus {
+    border-color: var(--main-color);
+  }
+</style>
