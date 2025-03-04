57f7e2a763fba519d31b4fbf34f5525a2d087694
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-04 16:51:57 +0900

    axios instance의 request 경로 수정

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,50 +1,49 @@
-import axios, { type AxiosInstance } from "axios";
+import axios, { type AxiosInstance } from 'axios'
 
-const LOCAL_STORAGE_REFRESH_TOKEN = "refresh_token";
+const LOCAL_STORAGE_REFRESH_TOKEN = 'refresh_token'
 
 const tokenAxiosInstance: AxiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true,
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
+  baseURL: import.meta.env.VITE_BACKEND_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
 tokenAxiosInstance.interceptors.request.use(
-    config => config,
-    error => Promise.reject(error)
+  (config) => config,
+  (error) => Promise.reject(error),
 )
 
 tokenAxiosInstance.interceptors.response.use(
-    response => response,
-    async error => {
-        console.log(error);
+  (response) => response,
+  async (error) => {
+    console.error(error)
 
-        const originalRequest = error.config;
-        if (originalRequest.url === "/member/renew-token") return Promise.reject(error);
-        
-        if (originalRequest._retry) return Promise.reject(error);
-        originalRequest._retry = true;
+    const originalRequest = error.config
+    if (originalRequest.url === '/api/member/renew-token') return Promise.reject(error)
 
-        if (error.response?.status === 401) {
+    if (originalRequest._retry) return Promise.reject(error)
+    originalRequest._retry = true
 
-            const refreshToken: string | null = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN);
-            if (!refreshToken) return Promise.reject(error);
+    if (error.response?.status === 401) {
+      const refreshToken: string | null = localStorage.getItem(LOCAL_STORAGE_REFRESH_TOKEN)
+      if (!refreshToken) return Promise.reject(error)
 
-            try {
-                await tokenAxiosInstance.post("/member/renew-token", { refreshToken });
-                const response = await tokenAxiosInstance.get("/member/refresh-token");
-                
-                localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN, response.data['refreshToken'])
+      try {
+        await tokenAxiosInstance.post('/api/member/renew-token', { refreshToken })
+        const response = await tokenAxiosInstance.get('/api/member/refresh-token')
 
-                return tokenAxiosInstance(error.config)
-            } catch (err) {
-                return Promise.reject(error);
-            }
-        }
+        localStorage.setItem(LOCAL_STORAGE_REFRESH_TOKEN, response.data['refreshToken'])
 
-        return Promise.reject(error);
+        return tokenAxiosInstance(error.config)
+      } catch (err) {
+        return Promise.reject(error)
+      }
     }
+
+    return Promise.reject(error)
+  },
 )
 
-export { tokenAxiosInstance, LOCAL_STORAGE_REFRESH_TOKEN };
+export { tokenAxiosInstance, LOCAL_STORAGE_REFRESH_TOKEN }
