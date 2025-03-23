7078c0b35545858f30dc18cffd2dbd1606374dfd
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-23 17:23:44 +0900

    fix: 로그인 페이지 css 수정

---
+++ src/components/Commons/LoginButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <button
+        @click="login ? login() : null"
+        class="mt-6 flex items-center cursor-pointer justify-between gap-3 border-1 border-gray-300 text-gray-800 px-6 py-3 rounded-lg w-60 mx-auto shadow-md transition-all hover:bg-gray-100 hover:border-gray-400"
+        :class="bgColor"
+      >
+        <img :src="icon" :alt="loginType" class="w-6 h-6">
+        <div class="flex-grow">
+          <span class="text-lg font-semibold"> {{ loginType }}</span>
+        </div>
+    </button>
+</template>
+<script lang="ts" setup>
+const props = defineProps({
+  login: {
+    type: Function,
+    required: false,
+    default: () => {}
+  },
+  loginType: {
+    type: String,
+    required: true,
+  },
+  bgColor: {
+    type: String,
+    required: true,
+    default: "bg-white"
+  },
+  icon: {
+    type: String,
+    required: true,
+  }
+})
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,23 +1,23 @@
 <template>
-  <div class="flex items-center justify-center min-h-screen">
+  <div class="flex  flex-grow items-center justify-center">
     <div class="bg-white shadow-lg rounded-2xl p-10 w-full max-w-md text-center">
       <h1 class="text-3xl font-bold text-gray-900">로그인</h1>
       <p class="text-gray-500 mt-2">Google 계정으로 간편하게 로그인하세요</p>
 
-      <button
-        @click="handleLogin"
-        class="mt-6 flex items-center justify-center gap-3 bg-purple-600 text-white px-6 py-3 rounded-xl w-full shadow-md transition-all hover:bg-white hover:text-purple-600 hover:border hover:border-purple-600"
-      >
-        <img :src="GoogleLoginIcon" alt="구글 로그인" class="w-6 h-6">
-        <span class="text-lg font-semibold">Google 로그인</span>
-      </button>
+      <LoginButton
+        :login="handleGoogleLogin"
+        loginType="Google 로그인"
+        bgColor="bg-white"
+        :icon="GoogleLoginIcon"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-  import GoogleLoginIcon from '../components/icons/web_light_sq_ctn.svg';
-  const handleLogin = () => {
+  import GoogleLoginIcon from '@/components/icons/g-logo.webp';
+  import LoginButton from '@/components/Commons/LoginButton.vue';
+  const handleGoogleLogin = () => {
     window.location.href = `${import.meta.env.VITE_BACKEND_URL}/oauth2/authorize/google`
   }
 </script>
