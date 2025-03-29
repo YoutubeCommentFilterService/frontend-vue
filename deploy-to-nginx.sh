0f1ab941f8c463a3f497691a90b500988425b89e
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-29 17:05:41 +0900

    fix: nginx로 배포하는 로직수정
    - 타임아웃 발생 시 build 및 배포를 진행하지 않도록 수정

--- deploy-to-nginx.sh
+++ deploy-to-nginx.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+NGINX_SSH_PORT=$NGINX_SSH_PORT
+NGINX_SSH_USER=$NGINX_SSH_USER
+NGINX_SSH_DOMAIN=$NGINX_SSH_DOMAIN
+
+NGINX_SSH_PATH="$NGINX_SSH_USER@$NGINX_SSH_DOMAIN"
+
+ssh -q -o ConnectTimeout=5 -p $NGINX_SSH_PORT $NGINX_SSH_PATH exit
+
+if [ $? -eq 0 ]; then
+    echo "SSH 연결 성공"
+else
+    echo "SSH 연결 실패. 스크립트를 종료합니다."
+    exit 1  # 연결 실패 시 종료
+fi
+
 bun run build
 
 if [ $? -ne 0 ]; then
   echo "❌ Build failed. Stopping deployment."
   exit 1
 fi
-
-NGINX_SSH_PORT=$NGINX_SSH_PORT
-NGINX_SSH_USER=$NGINX_SSH_USER
-NGINX_SSH_DOMAIN=$NGINX_SSH_DOMAIN
-
-NGINX_SSH_PATH="$NGINX_SSH_USER@$NGINX_SSH_DOMAIN"
 
 scp -P $NGINX_SSH_PORT -r ./dist "$NGINX_SSH_PATH:~/"
 ssh -p $NGINX_SSH_PORT "$NGINX_SSH_PATH" '. deploy-front.sh'
