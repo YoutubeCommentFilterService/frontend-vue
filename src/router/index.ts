015d1e45813b3cfe024909dd7f7b674ec17e40f5
PromotezCitizen <gkstkdgus821@gmail.com>
2025-03-30 22:37:45 +0900

    refactoring: import 경로 수정

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,13 +5,13 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomeView },
-    { path: '/videos', component: () => import('../views/VideosView.vue') },
-    { path: '/login', component: () => import('../views/LoginView.vue') },
-    { path: '/after-login', component: () => import('../views/AfterLoginView.vue') },
-    { path: '/comments', component: () => import('../views/CommentsView.vue') },
-    { path: '/my-page', component: () => import('../views/MyPageView.vue')},
-    { path: '/recent', component: () => import('../views/RecentCommentsView.vue')},
-    { path: '/privacy-google', component: () => import('@/views/ConcernedPrivacyGoogleView.vue')}
+    { path: '/videos', component: () => import('@/views/VideosView.vue') },
+    { path: '/login', component: () => import('@/views/LoginView.vue') },
+    { path: '/after-login', component: () => import('@/views/AfterLoginView.vue') },
+    { path: '/comments', component: () => import('@/views/CommentsView.vue') },
+    { path: '/my-page', component: () => import('@/views/MyPageView.vue') },
+    { path: '/recent', component: () => import('@/views/RecentCommentsView.vue') },
+    { path: '/privacy-google', component: () => import('@/views/ConcernedPrivacyGoogleView.vue') },
   ],
 })
 
