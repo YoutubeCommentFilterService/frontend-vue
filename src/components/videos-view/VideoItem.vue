cad6768b953c5467abc05491c80634fb9c13dc41
PromotezCitizen <gkstkdgus821@gmail.com>
2025-06-16 01:02:50 +0900

    동영상 조회 페이지의 로직 수정
    - 컴포넌트 분리
    - 사용하지 않는 로직 삭제
    - isLoading 초기값을 false로 하여 초기 로딩이 가능하도록 수정

---
+++ src/components/videos-view/VideoItem.vue
@@ -0,0 +1,62 @@
+<template>
+  <div
+    class="border border-gray-300 dark:border-gray-400 rounded-2xl overflow-hidden bg-white dark:bg-gray-400 shadow-md transition-transform duration-200 ease-in-out hover:translate-y-[-4px] hover:shadow-[0_4px_12px_rgba(0,0,0,0.15)]"
+    :class="{
+      'cursor-not-allowed': item.privacy === 'private',
+      'cursor-pointer': item.privacy !== 'private',
+    }"
+    :title="privacyTooltip"
+  >
+    <div class="relative">
+      <img :src="item.thumbnail" class="w-full h-[200px] object-cover" />
+      <div
+        v-if="item.privacy === 'private'"
+        class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-60 z-10"
+      >
+        <svg
+          width="80px"
+          height="80px"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            class="cls-1"
+            d="M16 8.00169L16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V8.00169M16 8.00169C15.7563 8 15.4907 8 15.2 8H8.8C8.50929 8 8.24373 8 8 8.00169M16 8.00169C17.1649 8.00979 17.8313 8.05658 18.362 8.32698C18.9265 8.6146 19.3854 9.07354 19.673 9.63803C20 10.2798 20 11.1198 20 12.8V16.2C20 17.8802 20 18.7202 19.673 19.362C19.3854 19.9265 18.9265 20.3854 18.362 20.673C17.7202 21 16.8802 21 15.2 21H8.8C7.11984 21 6.27976 21 5.63803 20.673C5.07354 20.3854 4.6146 19.9265 4.32698 19.362C4 18.7202 4 17.8802 4 16.2V12.8C4 11.1198 4 10.2798 4.32698 9.63803C4.6146 9.07354 5.07354 8.6146 5.63803 8.32698C6.16873 8.05658 6.83507 8.00979 8 8.00169M10 11V18M14 11V18M8.5 12.5H15.5M8.5 16.5H15.5"
+            stroke="#000000"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+      <div v-else-if="item.privacy === 'unlisted'" class="absolute top-2.5 right-2.5 z-20">
+        <svg width="30px" height="30px" viewBox="0 -1 510 515" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M476.335 35.664v.001c47.554 47.552 47.552 125.365.002 172.918l-101.729 101.73c-60.027 60.025-162.073 42.413-194.762-32.45 35.888-31.191 53.387-21.102 87.58-6.638 20.128 8.512 43.74 3.955 60.08-12.387l99.375-99.371c21.49-21.493 21.492-56.662 0-78.155-21.489-21.488-56.677-21.472-78.151 0l-71.278 71.28c-23.583-11.337-50.118-14.697-75.453-10.07a121.476 121.476 0 0118.767-24.207l82.651-82.65c47.554-47.551 125.365-47.555 172.918-.001zM35.664 476.334l.001.001c47.554 47.552 125.365 47.552 172.917 0l85.682-85.682a121.496 121.496 0 0019.325-25.157c-27.876 6.951-57.764 4.015-83.932-8.805l-70.192 70.19c-21.472 21.471-56.658 21.492-78.149 0-21.492-21.491-21.493-56.658 0-78.149l99.375-99.376c20.363-20.363 61.002-26.435 91.717 1.688 29.729-3.133 41.275-8.812 59.742-26.493-39.398-69.476-137.607-80.013-194.757-22.863L35.664 303.417c-47.552 47.553-47.552 125.364 0 172.917z"
+          />
+        </svg>
+      </div>
+    </div>
+    <p class="m-2 text-sm text-gray-800 truncate whitespace-nowrap overflow-hidden">
+      {{ item.title }}
+    </p>
+    <p class="mt-0 mb-3 ml-3 text-xs text-gray-500">업로드 일자: {{ publishedAt }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { VideoResource } from '@/types/video-interface'
+
+defineProps<{
+  item: VideoResource
+  publishedAt: string
+  privacyTooltip: string | undefined
+}>()
+</script>
+
+<style scoped>
+svg {
+  filter: drop-shadow(0px 0px 2px rgba(255, 255, 255));
+}
+</style>
--- src/views/VideosView.vue
+++ src/views/VideosView.vue
@@ -2,16 +2,19 @@
 import { onMounted, ref } from 'vue'
 import { useRouter } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import type { VideoResponseData, VideoResource } from '@/types/video-interface'
 import { tokenAxiosInstance } from '@/utils'
 import { useVideoStore } from '@/stores/video'
+
+import type { VideoResponseData, VideoResource } from '@/types/video-interface'
+
 import InfiniteScroll from '@/components/InfiniteScroll.vue'
+import VideoItem from '@/components/videos-view/VideoItem.vue'
 
 const router = useRouter()
 const authStore = useAuthStore()
 const videoStore = useVideoStore()
 
-const isLoading = ref<boolean>(true)
+const isLoading = ref<boolean>(false)
 
 const videoPrivacyTooltip: { [key: string]: string | undefined } = {
   private: '비공개 영상은 댓글 조회가 불가합니다 ㅠㅠ',
@@ -55,52 +58,39 @@ const refreshVideos = async (take: number = 50): Promise<VideoResponseData | voi
   if (data) videoStore.push(data)
 }
 
-const sleep = async (time: number) => {
-  await new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(null)
-    }, time)
-  })
-}
-
 const maxFetchNum = 50
 const loadMoreItem = async () => {
   try {
     const data = await fetchVideos(videoStore.videoPage, maxFetchNum)
-    // 스피너 디버그용
-    // await sleep(3000)
 
     if (data) {
       videoStore.push(data)
       return true
     }
-    return false
   } catch (err) {
     console.error(err)
-    return false
   }
-}
-const refreshItem = async () => {
-  await refreshVideos(maxFetchNum)
+  return false
 }
 
 const convertUTC2KST = (datetime: string | string[]): string => {
-  let date;
+  let date
   if (typeof datetime === 'object') {
-    date = new Date(Date.UTC(
-      parseInt(datetime[0]),         // year
-      parseInt(datetime[1]) - 1,     // month (0-based)
-      parseInt(datetime[2]),         // day
-      parseInt(datetime[3]) || 0,    // hour
-      parseInt(datetime[4]) || 0,    // minute
-      parseInt(datetime[5]) || 0     // second
-    ));
-  }
-  else {
+    date = new Date(
+      Date.UTC(
+        parseInt(datetime[0]), // year
+        parseInt(datetime[1]) - 1, // month (0-based)
+        parseInt(datetime[2]), // day
+        parseInt(datetime[3]) || 0, // hour
+        parseInt(datetime[4]) || 0, // minute
+        parseInt(datetime[5]) || 0, // second
+      ),
+    )
+  } else {
     if (!datetime.endsWith('Z')) datetime = datetime + 'Z'
     date = new Date(datetime)
   }
-  return date.toLocaleString('ko-kr', { timeZone: 'Asia/Seoul' });
+  return date.toLocaleString('ko-kr', { timeZone: 'Asia/Seoul' })
 }
 
 onMounted(() => {
@@ -116,72 +106,26 @@ onMounted(() => {
     <InfiniteScroll
       :is-last="videoStore.isLast"
       :load-more-item="loadMoreItem"
-      :refresh-item="refreshItem"
+      :refresh-item="refreshVideos"
       spinner-text="동영상을 불러오는 중입니다!!"
       v-model:is-loading="isLoading"
     >
-      <div 
+      <div
         class="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-4 p-5"
         v-if="videoStore.videoList.length > 0"
       >
-        <div
+        <VideoItem
           v-for="item in videoStore.videoList"
           :key="item.id"
-          class="border border-gray-300 dark:border-gray-400 rounded-2xl overflow-hidden bg-white dark:bg-gray-400 shadow-md transition-transform duration-200 ease-in-out hover:translate-y-[-4px] hover:shadow-[0_4px_12px_rgba(0,0,0,0.15)]"
-          :class="{
-            'cursor-not-allowed': item.privacy === 'private',
-            'cursor-pointer': item.privacy !== 'private',
-          }"
+          :item="item"
           @click="item.privacy !== 'private' && onVideoClick(item)"
-          :title="videoPrivacyTooltip[item.privacy]"
-        >
-          <div class="relative">
-            <img :src="item.thumbnail" class="w-full h-[200px] object-cover" />
-            <div
-              v-if="item.privacy === 'private'"
-              class="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-black bg-opacity-60 z-10"
-            >
-              <svg
-                width="80px"
-                height="80px"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  class="cls-1"
-                  d="M16 8.00169L16 7C16 4.79086 14.2091 3 12 3C9.79086 3 8 4.79086 8 7V8.00169M16 8.00169C15.7563 8 15.4907 8 15.2 8H8.8C8.50929 8 8.24373 8 8 8.00169M16 8.00169C17.1649 8.00979 17.8313 8.05658 18.362 8.32698C18.9265 8.6146 19.3854 9.07354 19.673 9.63803C20 10.2798 20 11.1198 20 12.8V16.2C20 17.8802 20 18.7202 19.673 19.362C19.3854 19.9265 18.9265 20.3854 18.362 20.673C17.7202 21 16.8802 21 15.2 21H8.8C7.11984 21 6.27976 21 5.63803 20.673C5.07354 20.3854 4.6146 19.9265 4.32698 19.362C4 18.7202 4 17.8802 4 16.2V12.8C4 11.1198 4 10.2798 4.32698 9.63803C4.6146 9.07354 5.07354 8.6146 5.63803 8.32698C6.16873 8.05658 6.83507 8.00979 8 8.00169M10 11V18M14 11V18M8.5 12.5H15.5M8.5 16.5H15.5"
-                  stroke="#000000"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-            </div>
-            <div v-else-if="item.privacy === 'unlisted'" class="absolute top-2.5 right-2.5 z-20">
-              <svg
-                width="30px"
-                height="30px"
-                viewBox="0 -1 510 515"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M476.335 35.664v.001c47.554 47.552 47.552 125.365.002 172.918l-101.729 101.73c-60.027 60.025-162.073 42.413-194.762-32.45 35.888-31.191 53.387-21.102 87.58-6.638 20.128 8.512 43.74 3.955 60.08-12.387l99.375-99.371c21.49-21.493 21.492-56.662 0-78.155-21.489-21.488-56.677-21.472-78.151 0l-71.278 71.28c-23.583-11.337-50.118-14.697-75.453-10.07a121.476 121.476 0 0118.767-24.207l82.651-82.65c47.554-47.551 125.365-47.555 172.918-.001zM35.664 476.334l.001.001c47.554 47.552 125.365 47.552 172.917 0l85.682-85.682a121.496 121.496 0 0019.325-25.157c-27.876 6.951-57.764 4.015-83.932-8.805l-70.192 70.19c-21.472 21.471-56.658 21.492-78.149 0-21.492-21.491-21.493-56.658 0-78.149l99.375-99.376c20.363-20.363 61.002-26.435 91.717 1.688 29.729-3.133 41.275-8.812 59.742-26.493-39.398-69.476-137.607-80.013-194.757-22.863L35.664 303.417c-47.552 47.553-47.552 125.364 0 172.917z"
-                />
-              </svg>
-            </div>
-          </div>
-          <p class="m-2 text-sm text-gray-800 truncate whitespace-nowrap overflow-hidden">
-            {{ item.title }}
-          </p>
-          <p class="mt-0 mb-3 ml-3 text-xs text-gray-500">
-            업로드 일자: {{ convertUTC2KST(item.publishedAt) }}
-          </p>
-        </div>
+          :published-at="convertUTC2KST(item.publishedAt)"
+          :privacy-tooltip="videoPrivacyTooltip[item.privacy]"
+        />
       </div>
-      <div 
-        class="items-center  justify-center flex h-full"
-        v-else-if="!isLoading"
+      <div
+        class="items-center justify-center flex h-full"
+        v-else-if="!isLoading && videoStore.videoList.length === 0"
       >
         <p v-if="authStore.profile.hasYoutubeAccess" class="">동영상이 존재하지 않습니다.</p>
         <p v-else>유튜브 계정 연동을 먼저 진행해 주세요!</p>
@@ -189,9 +133,3 @@ onMounted(() => {
     </InfiniteScroll>
   </div>
 </template>
-
-<style scoped>
-svg {
-  filter: drop-shadow(0px 0px 2px rgba(255, 255, 255));
-}
-</style>
